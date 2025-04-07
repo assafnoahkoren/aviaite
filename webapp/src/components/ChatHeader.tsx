502d1deae58f72b4a24db8c19243cfb06404297d
Assaf Koren <assafnoahkoren@gmail.com>
2025-04-07 09:50:39 +0300

    .

--- webapp/src/components/ChatHeader.tsx
+++ webapp/src/components/ChatHeader.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-interface ChatHeaderProps {
-}
 
-const ChatHeader: React.FC<ChatHeaderProps> = (props: ChatHeaderProps) => {
+const ChatHeader: React.FC = () => {
   return (
     <div className="flex items-center px-4 py-3 border-b border-gray-200 bg-gray-1">
       <h1 className="text-xl font-400 text-blue-500 m-0 ">
